d575dd09695157eb7ebcc8032ab1b7194d2867b4
22051988Di <vitalii.soloninko@gmail.com>
2024-08-06 16:18:31 +0200

    v0.2.0	PostgreSQL. Sequelize. Connecting to the database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common'
+import { SequelizeModule } from '@nestjs/sequelize'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
 	controllers: [AppController],
 	providers: [AppService],
+	imports: [
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: 'localhost',
+			port: 5500,
+			username: 'postgres',
+			password: 'admin',
+			database: 'nest-rest-api',
+			models: [],
+			autoLoadModels: true,
+		}),
+	],
 })
 export class AppModule {}
