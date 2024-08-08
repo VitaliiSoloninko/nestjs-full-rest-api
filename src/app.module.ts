3d00439108f500aa4b9e8eb20f0228014d63b32d
22051988Di <vitalii.soloninko@gmail.com>
2024-08-08 16:15:17 +0200

    v0.2.1	Create UserModule. UserService. UserController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { UsersModule } from './users/users.module';
 
 @Module({
-	controllers: [AppController],
-	providers: [AppService],
+	controllers: [],
+	providers: [],
 	imports: [
 		SequelizeModule.forRoot({
 			dialect: 'postgres',
@@ -17,6 +16,7 @@ import { AppService } from './app.service'
 			models: [],
 			autoLoadModels: true,
 		}),
+		UsersModule,
 	],
 })
 export class AppModule {}
