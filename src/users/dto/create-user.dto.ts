6e94bffd31f5d8f20984832f3cd7b44941820c14
22051988Di <vitalii.soloninko@gmail.com>
2024-08-09 19:37:43 +0200

    v0.3.3	Swagger. Model, dto, controller documents @ApiProperty  @ApiTags

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export class CreateUserDto {
+	@ApiProperty({ example: 'user@gmail.com', description: 'E-mail' })
 	readonly email: string
+	@ApiProperty({ example: '12345678', description: 'Password' })
 	readonly password: string
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,10 @@
 import { Body, Controller, Get, Post } from '@nestjs/common'
-import { ApiOperation, ApiResponse } from '@nestjs/swagger'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UsersService } from './users.service'
 import { User } from './users.model'
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
 	constructor(private userService: UsersService) {}
