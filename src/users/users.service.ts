0cbcfe9cb2fb88790f5a9a91346511516030002a
22051988Di <vitalii.soloninko@gmail.com>
2024-08-10 13:51:54 +0200

    v0.4.6	Create standart role: user. Export roles.module and connect in users.service, users.module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 import { CreateUserDto } from './dto/create-user.dto'
 import { User } from './users.model'
+import { RolesService } from 'src/roles/roles.service'
 
 @Injectable()
 export class UsersService {
-	constructor(@InjectModel(User) private userRepository: typeof User) {}
+	constructor(
+		@InjectModel(User) private userRepository: typeof User,
+		private roleService: RolesService
+	) {}
 
 	async createUser(dto: CreateUserDto) {
 		const user = await this.userRepository.create(dto)
