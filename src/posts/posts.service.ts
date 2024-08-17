ecbae6c8324f9e21134468f669ac925681550b89
22051988Di <vitalii.soloninko@gmail.com>
2024-08-17 12:49:35 +0200

    v0.9.11	File service. Import path - work with way, fs - work with files, uuid - generate name files, dont work correctly

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 import { CreatePostDto } from './dto/create-post.dto'
 import { Post } from './posts.model'
+import { FilesService } from 'src/files/files.service'
 
 @Injectable()
 export class PostsService {
-	constructor(@InjectModel(Post) private postRepository: typeof Post) {}
+	constructor(
+		@InjectModel(Post) private postRepository: typeof Post,
+		private fileService: FilesService
+	) {}
 
 	async create(dto: CreatePostDto, image: any) {
-		const fileName = '12345'
+		const fileName = await this.fileService.createFile(image)
 		const post = await this.postRepository.create({ ...dto, image: fileName })
 		return post
 	}
