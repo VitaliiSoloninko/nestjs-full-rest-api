824fa4c5cc31a53fb083eef3a5e3fbc897393da1
22051988Di <vitalii.soloninko@gmail.com>
2024-08-17 12:20:20 +0200

    v0.9.9	Create post service logic and controller

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/sequelize'
+import { CreatePostDto } from './dto/create-post.dto'
+import { Post } from './posts.model'
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+	constructor(@InjectModel(Post) private postRepository: typeof Post) {}
+
+	async create(dto: CreatePostDto, image: any) {
+		const fileName = '12345'
+		const post = await this.postRepository.create({ ...dto, image: fileName })
+		return post
+	}
+}
