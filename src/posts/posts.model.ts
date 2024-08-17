3cf86e6aa18da54c56fba56fe4298086b55790da
22051988Di <vitalii.soloninko@gmail.com>
2024-08-17 11:52:14 +0200

    v0.9.7	Create post model. @BelongsTo. User model @HasMany. App module add Post to models

---
+++ src/posts/posts.model.ts
@@ -0,0 +1,49 @@
+import { ApiProperty } from '@nestjs/swagger'
+import {
+	BelongsTo,
+	BelongsToMany,
+	Column,
+	DataType,
+	Model,
+	Table,
+} from 'sequelize-typescript'
+import { Role } from 'src/roles/roles.model'
+import { UserRoles } from 'src/roles/user-roles.model'
+import { User } from 'src/users/users.model'
+
+interface PostCreationAttrs {
+	email: string
+	password: string
+}
+
+@Table({ tableName: 'posts' })
+export class Post extends Model<Post, PostCreationAttrs> {
+	@Column({
+		type: DataType.INTEGER,
+		unique: true,
+		autoIncrement: true,
+		primaryKey: true,
+	})
+	id: number
+
+	@Column({
+		type: DataType.STRING,
+		unique: true,
+		allowNull: false,
+	})
+	title: string
+
+	@Column({
+		type: DataType.STRING,
+		allowNull: false,
+	})
+	content: string
+
+	@Column({
+		type: DataType.STRING,
+	})
+	image: string
+
+	@BelongsTo(() => User)
+	autor: User
+}
